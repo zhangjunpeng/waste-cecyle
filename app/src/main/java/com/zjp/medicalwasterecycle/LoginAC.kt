d258f684afd74e96b68ca3650cbb0e987f1a680a
zhangjunpeng <zhangjunpeng@nextmar.com>
2022-04-12 18:09:06 +0800

    feat:login

---
+++ app/src/main/java/com/zjp/medicalwasterecycle/LoginAC.kt
@@ -0,0 +1,28 @@
+package com.zjp.medicalwasterecycle
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.zjp.medicalwasterecycle.databinding.ActivityLoginBinding
+
+class LoginAC : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+
+    }
+
+
+}
